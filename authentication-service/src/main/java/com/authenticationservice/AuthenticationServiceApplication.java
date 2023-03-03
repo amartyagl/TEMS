ba8764df44708e34760614622c08f647eb4d8ab2
amartya.singh <amartya.singh@globallogic.com>
2023-03-03 22:46:32 +0530

    adding authentication service

--- authentication-service/src/main/java/com/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/authenticationservice/AuthenticationServiceApplication.java
@@ -2,10 +2,8 @@ package com.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
